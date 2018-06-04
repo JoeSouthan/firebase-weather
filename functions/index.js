6687fcb52c1a3a3fa70a4824e238cb7e55eec049
Joe Southan <joseph@southanuk.co.uk>
2018-06-04 19:44:55 +0100

    Running totals

--- functions/index.js
+++ functions/index.js
@@ -7,24 +7,25 @@ admin.initializeApp(functions.config().firebase);
 
 const db = admin.database();
 
+// Want to eventually calculate IAC
+// https://github.com/pimoroni/bme680/blob/master/examples/indoor-air-quality.py#L54
+const formattedData = ({ temperature, pressure, humidity, gas, timestamp }) => (
+  {
+    temperature,
+    pressure,
+    humidity,
+    gas,
+    timestamp
+  }
+)
+
 exports.receiveTelemetry = functions.pubsub
   .topic('telemetry')
   .onPublish(event => {
     const attributes = event.attributes;
     const message = event.json;
-
     const deviceId = attributes['deviceId'];
-
-    // Want to eventually calculate IAC
-    // https://github.com/pimoroni/bme680/blob/master/examples/indoor-air-quality.py#L54
-    const data = {
-      temperature: message.temperature,
-      pressure: message.pressure,
-      humidity: message.humidity,
-      gas: message.gas,
-      deviceId: deviceId,
-      timestamp: message.timestamp
-    };
+    const data = Object.assign({}, formattedData(message), { deviceId })
 
     if (
       message.humidity < 0 ||
@@ -37,17 +38,30 @@ exports.receiveTelemetry = functions.pubsub
 
     return Promise.all([
       insertIntoBigquery(data),
-      updateCurrentDataFirebase(data)
+      updateCurrentDataFirebase(data),
+      updateRunningTotals(data),
     ]);
   });
 
 function updateCurrentDataFirebase(data) {
-  return db.ref(`/devices/${data.deviceId}`).set({
-    temperature: data.temperature,
-    pressure: data.pressure,
-    humidity: data.humidity,
-    gas: data.gas,
-    timestamp: data.timestamp
+  return db.ref(`/devices/${data.deviceId}`).set(formattedData(data));
+}
+
+function updateRunningTotals(data) {
+  const ref = db.ref(`/latest_readings/${data.deviceId}`)
+  const currentTime = new Date()
+  const cutoff = currentTime - (24 * 60 * 60 * 1000)
+  const oldItemsQuery = ref.orderByChild('timestamp').endAt(cutoff)
+
+  // Set new data
+  db.ref(`/latest_readings/${data.deviceId}/${currentTime.getTime()}`).set(formattedData(data))
+
+  return oldItemsQuery.once('value', (snapshot) => {
+    let updates = {};
+    snapshot.forEach((child) => {
+      updates[child.key] = null
+    });
+    return ref.update(updates);
   });
 }
 
